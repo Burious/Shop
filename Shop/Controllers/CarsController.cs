55517045b369c301226199e378bdbab7336f9a16
Artsemi Buracheuski <artemy.burr@gmail.com>
2021-10-07 13:48:23 +0300

    Add links on electro cars and fuel cars on the top of page

--- Shop/Controllers/CarsController.cs
+++ Shop/Controllers/CarsController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Data.Interfaces;
+using Shop.Data.Models;
 using Shop.ViewModels;
 
 namespace Shop.Controllers
@@ -19,14 +21,40 @@ namespace Shop.Controllers
             _allCategories = iCarsCategory;
         }
 
-        public ViewResult List()
+        [Route("Cars/List")]
+        [Route("Cars/List/{category}")]
+        public ViewResult List(string category)
         {
-            ViewBag.Title = "Страница с автомобилями";
-            CarsListViewModel obj = new CarsListViewModel();
-            obj.allCars = _allCars.Cars;
-            obj.currCategory = "Автомобили";
+            string _category = category;
+            IEnumerable<Car> cars = null;
+            string currCategory = "";
+            if (string.IsNullOrEmpty(category))
+            {
+                cars = _allCars.Cars.OrderBy(i => i.id);
+            }
+            else
+            {
+                if (string.Equals("electro", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    cars = _allCars.Cars.Where((i => i.Category.categoryName.Equals("Электромобили"))).OrderBy(i=>i.id);
+                    currCategory = "Электромобили";
+                }
+                else if (string.Equals("fuel", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    cars = _allCars.Cars.Where((i => i.Category.categoryName.Equals("Классические автомобили"))).OrderBy(i=>i.id);
+                    currCategory = "Автомобили с ДВС";
+                }
+            }
 
-            return View(obj);
+            var carObj = new CarsListViewModel()
+            {
+                allCars = cars,
+                currCategory = currCategory
+            };
+            ViewBag.Title = "Страница с автомобилями";
+
+
+            return View(carObj);
         }
     }
 }
